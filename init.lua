3cc65024ffe4c43fd317576f3315ad5fc3dbfa2c
Dean Fogarty <angrytongan@gmail.com>
2026-02-15 14:07:33 +1000

    Updates.

--- init.lua
+++ init.lua
@@ -79,3 +79,14 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		})
 	end,
 })
+
+-- Markdown wrapping.
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "markdown",
+    callback = function()
+        vim.opt_local.wrap = true
+        vim.opt_local.textwidth = 80
+        vim.opt_local.linebreak = true
+        vim.opt_local.formatoptions:append("t")
+    end,
+})
